a1f7a4bbb6ff93f6c32b9969201cf27efd8cf065
Kavi Raj <feddev66@gmail.com>
2022-01-12 01:18:03 +0545

    Paradise IT - blog title update

--- www/wp-content/themes/paradiseit/header.php
+++ www/wp-content/themes/paradiseit/header.php
@@ -84,40 +84,48 @@
 		</header>
 		<main class="site-content">
 			<?php if (!is_404()) {
-				if (is_front_page()) { ?>
+				if (is_front_page()) {
+					$banner_info = get_field('banner_info');
+					$banner_image = get_field('banner_image'); ?>
 					<div class="main-banner">
 						<div class="d-table">
 							<div class="d-table-cell">
 								<div class="container">
 									<div class="row h-100 justify-content-center align-items-center">
-										<div class="col-lg-5">
-											<div class="hero-content">
-												<h1>Secure IT Solutions for a more secure environment</h1>
-												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-													incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-													gravida.</p>
-												<a href="contact.html" class="btn btn-primary">Get Started</a>
+										<?php if ($banner_info) { ?>
+											<div class="col-lg-5">
+												<div class="hero-content">
+													<?= apply_filters('content', $banner_info); ?>
+												</div>
 											</div>
-										</div>
-										<div class="col-lg-6 offset-lg-1">
-											<div class="banner-image">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/man.png" class="wow fadeInDown" data-wow-delay="0.6s" alt="man">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/code.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="code">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/carpet.png" class="wow fadeInLeft" data-wow-delay="0.6s" alt="carpet">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/bin.png" class="wow zoomIn" data-wow-delay="0.6s" alt="bin">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/book.png" class="wow bounceIn" data-wow-delay="0.6s" alt="book">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/dekstop.png" class="wow fadeInDown" data-wow-delay="0.6s" alt="dekstop">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/dot.png" class="wow zoomIn" data-wow-delay="0.6s" alt="dot">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/flower-top-big.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="flower-top-big">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/flower-top.png" class="wow rotateIn" data-wow-delay="0.6s" alt="flower-top">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/keyboard.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="keyboard">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/pen.png" class="wow zoomIn" data-wow-delay="0.6s" alt="pen">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/table.png" class="wow zoomIn" data-wow-delay="0.6s" alt="table">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/tea-cup.png" class="wow fadeInLeft" data-wow-delay="0.6s" alt="tea-cup">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/headphone.png" class="wow rollIn" data-wow-delay="0.6s" alt="headphone">
-												<img src="<?= get_template_directory_uri(); ?>/img/banner-image/main-pic.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="main-pic">
+										<?php }
+										if ($banner_image) { ?>
+											<div class="col-lg-6 offset-lg-1">
+												<div class="banner-image">
+													<?= get_acf_image($banner_image, 'wow fadeInUp', '', '0.6'); ?>
+												</div>
 											</div>
-										</div>
+										<?php } else { ?>
+											<div class="col-lg-6 offset-lg-1">
+												<div class="banner-image">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/man.png" class="wow fadeInDown" data-wow-delay="0.6s" alt="man">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/code.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="code">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/carpet.png" class="wow fadeInLeft" data-wow-delay="0.6s" alt="carpet">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/bin.png" class="wow zoomIn" data-wow-delay="0.6s" alt="bin">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/book.png" class="wow bounceIn" data-wow-delay="0.6s" alt="book">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/dekstop.png" class="wow fadeInDown" data-wow-delay="0.6s" alt="dekstop">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/dot.png" class="wow zoomIn" data-wow-delay="0.6s" alt="dot">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/flower-top-big.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="flower-top-big">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/flower-top.png" class="wow rotateIn" data-wow-delay="0.6s" alt="flower-top">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/keyboard.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="keyboard">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/pen.png" class="wow zoomIn" data-wow-delay="0.6s" alt="pen">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/table.png" class="wow zoomIn" data-wow-delay="0.6s" alt="table">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/tea-cup.png" class="wow fadeInLeft" data-wow-delay="0.6s" alt="tea-cup">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/headphone.png" class="wow rollIn" data-wow-delay="0.6s" alt="headphone">
+													<img src="<?= get_template_directory_uri(); ?>/img/banner-image/main-pic.png" class="wow fadeInUp" data-wow-delay="0.6s" alt="main-pic">
+												</div>
+											</div>
+										<?php } ?>
 									</div>
 								</div>
 							</div>
@@ -131,12 +139,13 @@
 						<div class="shape7"><img src="<?= get_template_directory_uri(); ?>/img/shape4.svg" alt="shape"></div>
 						<div class="shape8 rotateme"><img src="<?= get_template_directory_uri(); ?>/img/shape2.svg" alt="shape"></div>
 					</div>
-				<?php } else { ?>
+				<?php } else {
+					$title = is_home() ? 'Blog' : get_the_title; ?>
 					<div class="page-title-area">
 						<div class="d-table">
 							<div class="d-table-cell">
 								<div class="container">
-									<h1><?= get_the_title(); ?></h1>
+									<h1><?= $title; ?></h1>
 								</div>
 							</div>
 						</div>
