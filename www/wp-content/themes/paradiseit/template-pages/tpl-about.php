cc7a90f159011a3084bf7389216d8c3adac13d4e
Kavi Raj <feddev66@gmail.com>
2022-01-17 19:09:12 +0545

    Paradise IT - about page client update

--- www/wp-content/themes/paradiseit/template-pages/tpl-about.php
+++ www/wp-content/themes/paradiseit/template-pages/tpl-about.php
@@ -27,52 +27,25 @@ while (have_posts()) {
             } ?>
         </div>
     </div>
-
-    <?php $get_teams = kk_get_custom_post_type(-1, 'pit_team', 'DESC', 'date');
-    if ($get_teams) { ?>
-        <div class="team-area ptb-80 bg-f9f6f6">
+    <?php $get_clients = kk_get_custom_post_type(-1, 'pit_clients', 'DESC', 'date');
+    if ($get_clients) { ?>
+        <div class="privacy-policy-area ptb-80">
             <div class="container">
-                <div class="section-title">
-                    <h2>Our Awesome Team</h2>
-                    <div class="bar"></div>
-                    <p>Experienced and knowledgeable staffs, is like a magician they used to change everything if they want. </p>
-                </div>
-            </div>
-            <?php if ($get_teams) { ?>
-                <div class="container-fluid p-0">
-                    <div class="team-slides owl-carousel owl-theme">
-                        <?php foreach ($get_teams as $single_team) {
-                            $designation = get_field('designation', $single_team->ID);
-                            $team_social = array(
-                                array(get_field('facebook', $single_team->ID), 'facebook'),
-                                array(get_field('twitter', $single_team->ID), 'twitter'),
-                                array(get_field('linkedin', $single_team->ID), 'linkedin'),
-                                array(get_field('gitlab', $single_team->ID), 'gitlab'),
-                            ); ?>
-                            <div class="single-team">
-                                <div class="team-image">
-                                    <?= get_thumbnail_url_and_alt_text($single_team->ID, home_url('media/team.png'), 'team-thumb'); ?>
+                <div class="partner-area partner-section">
+                    <div class="partner-inner">
+                        <div class="row">
+                            <?php foreach ($get_clients as $clients) { ?>
+                                <div class="col-lg-2 col-md-3 col-6 col-sm-4">
+                                    <a href="<?= get_field('client_web_url', $clients->ID) ?: '#'; ?>">
+                                        <?= get_acf_image(get_field('image_one', $clients->ID)); ?>
+                                        <?= get_acf_image(get_field('image_two', $clients->ID)); ?>
+                                    </a>
                                 </div>
-                                <div class="team-content">
-                                    <div class="team-info">
-                                        <h3><?= $single_team->post_title; ?></h3>
-                                        <?= check_if_exists($designation, 'span'); ?>
-                                    </div>
-                                    <ul>
-                                        <?php for ($i = 0; $i < count($team_social); $i++) { ?>
-                                            <li>
-                                                <a href="<?= $team_social[$i][0] ?: '#'; ?>" target="_blank"><i data-feather="<?= $team_social[$i][1]; ?>"></i>
-                                                </a>
-                                            </li>
-                                        <?php } ?>
-                                    </ul>
-                                    <?= check_if_exists($single_team->post_content, 'p'); ?>
-                                </div>
-                            </div>
-                        <?php } ?>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
-            <?php } ?>
+            </div>
         </div>
 <?php }
 }
