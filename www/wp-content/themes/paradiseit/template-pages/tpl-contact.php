00a05190707a914436445f22306e32395832256f
Kavi Raj <feddev66@gmail.com>
2022-02-03 18:35:04 +0545

    Paradise: contact update

--- www/wp-content/themes/paradiseit/template-pages/tpl-contact.php
+++ www/wp-content/themes/paradiseit/template-pages/tpl-contact.php
@@ -18,23 +18,5 @@ while (have_posts()) {
 			<iframe src="<?= $map_url ?>"></iframe>
 		</div>
 	<?php } ?>
-
-	<div class="contact-area ptb-80">
-		<div class="container">
-			<div class="section-title">
-				<h2>Get In Touch With Us</h2>
-				<div class="bar"></div>
-				<p>Anything On your Mind. We’ll Be Glad To Assist You!</p>
-			</div>
-			<div class="row h-100 justify-content-center align-items-center">
-				<div class="col-lg-6 col-md-12">
-					<?= get_thumbnail_url_and_alt_text(get_the_ID()) ?>
-				</div>
-				<div class="col-lg-6 col-md-12 justify-content-center">
-					<?= do_shortcode('[wpforms id="228"]'); ?>
-				</div>
-			</div>
-		</div>
-	</div>
 <?php }
 get_footer();
